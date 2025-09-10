c4a0983a96e1dbbe915b5b5b9186244b5080b6d8
Annand Virk <annandvirk@gmail.com>
2025-09-09 20:27:57 -0400

    Add title screen scene and script for game menu navigation
    - Created title_screen.tscn to define the layout and UI elements for the game menu, including title, start, and quit options.
    - Implemented title_screen.gd to handle user input for menu navigation and selection, allowing players to start the game or quit.
    - Integrated pointer functionality to visually indicate the selected menu option.

---
+++ scripts/title_screen.gd
@@ -0,0 +1,42 @@
+extends Control
+
+@onready var title: RichTextLabel = %Title
+@onready var start: RichTextLabel = %Start
+@onready var quit: RichTextLabel = %Quit
+@onready var pointer: TextureRect = %Pointer
+
+var option_index: int = 0
+
+
+func _ready() -> void:
+	pointer.position = Vector2(457, 528)
+	pointer.scale = Vector2(0.75, 0.75)
+	pointer.texture = load("res://assets/ui/HandRight.png")
+	title.text = "[b]CHIMP OUT[/b]"
+	start.text = "START"
+	quit.text = "QUIT"
+
+
+func _process(_delta: float) -> void:
+	_move_pointer()
+	_process_selection()
+
+
+func _move_pointer() -> void:
+	if Input.is_action_just_pressed("move_up") or Input.is_action_just_pressed("move_down"):
+		option_index = 1 if option_index == 0 else 0
+
+	match option_index:
+		0:
+			pointer.position = Vector2(457, 528)
+		1:
+			pointer.position = Vector2(457, 569)
+
+
+func _process_selection() -> void:
+	if Input.is_action_just_pressed("start") or Input.is_action_just_pressed("select"):
+		match option_index:
+			0:
+				GSM.state = GSM.States.GAME
+			1:
+				get_tree().quit()
