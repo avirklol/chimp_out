8ee8e5f06d7411b98212fe5eea531f0b88e94147
Annand Virk <annandvirk@gmail.com>
2025-09-17 22:41:09 -0400

    Update player_manager.gd to enhance player connection handling
    - Modified player_joined and player_left signals to include player_index for better tracking of player states.
    - Improved input handling logic to manage player joins and leaves more effectively, including device ID management.
    - Added a new _remove_player function to streamline player disconnection processes and ensure proper cleanup of player data.
    - Implemented get_player_id function to retrieve player ID for all input events.

--- scripts/player_manager.gd
+++ scripts/player_manager.gd
@@ -1,8 +1,8 @@
 extends Node
 class_name PlayerManager
 
-signal player_joined(player_id: int, device_id: int)
-signal player_left(player_id: int)
+signal player_joined(player_id: int, device_id: int, player_index: int)
+signal player_left(player_id: int, player_index: int)
 
 @export var max_players: int = 4
 
@@ -15,74 +15,102 @@ var player_number: int = 1
 
 
 func _ready() -> void:
-    action_list = InputMap.get_actions()
+	action_list = InputMap.get_actions()
 
-    for player in range(max_players):
-        players.append({ "player_id": player_number, "device_id": null, "joined": false, "monkey": null })
-        player_number += 1
+	for player in range(max_players):
+		players.append({ "player_id": player_number, "device_id": null, "joined": false, "monkey": null })
+		player_number += 1
 
-    Input.joy_connection_changed.connect(_on_joy_connection_changed)
+	Input.joy_connection_changed.connect(_on_joy_connection_changed)
 
 func _unhandled_input(event: InputEvent) -> void:
-    if GM.state == GM.States.PLAYER_SELECTION:
-        if event is InputEventJoypadButton:
-            if event.button_index == JOY_BUTTON_START:
-                if !device_player_map.has(event.device):
-                    for player in players:
-                        if !player["joined"]:
-                            var player_id: int = player["player_id"]
+	var device_id: int = event.device
+	var player_id: int = get_player_id(device_id)
 
-                            device_player_map[event.device] = player_id
+	if GM.state == GM.States.PLAYER_SELECTION:
+		if event is InputEventJoypadButton:
+			if event.button_index == JOY_BUTTON_START:
+				if !device_player_map.has(device_id):
+					for player in players:
+						if !player["joined"]:
+							var player_index: int = players.find(player)
 
-                            player["joined"] = true
-                            player["device_id"] = event.device
-                            player["monkey"] = monkey_scene.instantiate()
-                            player["monkey"].player_id = player_id
-                            player["monkey"].device_id = event.device
-                            player["monkey"].name = "Player %d" % player_id
+							player_id = player["player_id"]
+							device_player_map[device_id] = player_id
 
-                            _build_input_map(player_id, event.device)
+							player["joined"] = true
+							player["device_id"] = device_id
+							player["monkey"] = monkey_scene.instantiate()
+							player["monkey"].player_id = player_id
+							player["monkey"].device_id = device_id
+							player["monkey"].name = "Player %d" % player_id
 
-                            player_joined.emit(player_id, event.device)
+							_build_input_map(player_id, device_id)
 
-                            break
-    else:
-        return # MAX PLAYERS
+							player_joined.emit(player_id, device_id, player_index)
+
+							break
+
+		if event.is_action_pressed("back%d" % player_id):
+			_remove_player(player_id, device_id)
+
+	else:
+		return # MAX PLAYERS
 
 
-func _on_joy_connection_changed(device: int, connected: bool) -> void:
-    if !connected and device_player_map.has(device):
-        var player_id: int = device_player_map[device]
+func _on_joy_connection_changed(device_id: int, connected: bool) -> void:
+	if !connected and device_player_map.has(device_id):
+		var player_id: int = device_player_map[device_id]
 
-        for player in players:
-            if player["player_id"] == player_id:
-                player["joined"] = false
-                player["device_id"] = null
-                player["monkey"] = null
+		_remove_player(player_id, device_id)
 
-                break
 
-        device_player_map.erase(device)
+func _remove_player(player_id: int, device_id: int) -> void:
+	var player_index: int
 
-        player_left.emit(player_id)
+	for player in players:
+		if player["player_id"] == player_id:
+			player["joined"] = false
+			player["device_id"] = null
+			player["monkey"] = null
+			player_index = players.find(player)
+
+			break
+
+	device_player_map.erase(device_id)
+
+	for action in action_list:
+		var player_action = action + str(player_id)
+
+		if InputMap.has_action(player_action):
+			InputMap.erase_action(player_action)
+
+	player_left.emit(player_id, player_index)
 
 
 func _build_input_map(player_id: int, device_id: int) -> void:
-    var action_event_list:Array[InputEvent]
-    var current_action:StringName
-    var current_event:InputEvent
+	var action_event_list:Array[InputEvent]
+	var current_action:StringName
+	var current_event:InputEvent
 
-    for action in range(action_list.size()):
-        if (action_list[action].begins_with("ui_")):
-            continue
+	for action in range(action_list.size()):
+		if (action_list[action].begins_with("ui_")):
+			continue
 
-        current_action = action_list[action]+str(player_id)
-        InputMap.add_action(current_action)
+		current_action = action_list[action] + str(player_id)
+		InputMap.add_action(current_action)
 
-        action_event_list = InputMap.action_get_events(action_list[action])
+		action_event_list = InputMap.action_get_events(action_list[action])
 
-        for event in range(action_event_list.size()):
-            current_event = action_event_list[event].duplicate(true)
-            current_event.set_device(device_id)
-            print(current_event.device)
-            InputMap.action_add_event(current_action,current_event)
+		for event in range(action_event_list.size()):
+			current_event = action_event_list[event].duplicate(true)
+			current_event.set_device(device_id)
+			print(current_event.device)
+			InputMap.action_add_event(current_action,current_event)
+
+
+func get_player_id(device_id: int) -> int:
+	if device_player_map.has(device_id):
+		return device_player_map[device_id]
+	else:
+		return 0
