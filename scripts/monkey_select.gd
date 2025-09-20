5a5fe868ab237263fd135a107c96b06841774a80
Annand Virk <annandvirk@gmail.com>
2025-09-20 17:11:54 -0400

    Implemented player ready/unready input logic and monkey sprite selection.

--- scripts/monkey_select.gd
+++ scripts/monkey_select.gd
@@ -1,37 +1,72 @@
 extends Control
 class_name MonkeySelect
 
+@export var checkbox_textures: Array[Resource]
+
 var player_join_labels: Array[Node]
 var player_joined_sprite_location: Array[Node]
 var player_ready_labels: Array[Node]
+var player_ready_checkboxes: Array[Node] = []
+var ready_players: int = 0
 
 
 func _ready() -> void:
-	GM.state = GM.States.PLAYER_SELECTION
-
 	PM.player_joined.connect(_on_player_joined)
 	PM.player_left.connect(_on_player_left)
 
 	player_join_labels = %PlayerJoin.get_children()
 	player_joined_sprite_location = %MonkeySprite.get_children()
-	player_ready_labels = %PlayerReady.get_children()
+	player_ready_labels = %PlayerReady.get_children().filter(func(child: Node): return child is RichTextLabel)
+	player_ready_checkboxes = %PlayerReady.get_children().filter(func(child: Node): return child is TextureRect)
+
 
 	for label in player_join_labels:
 		label.visible = true
+
 	for label in player_ready_labels:
 		label.visible = false
+		player_ready_checkboxes.append(label.get_child(0))
+
+	for checkbox in player_ready_checkboxes:
+		checkbox.texture = checkbox_textures[0]
+
+		var checked = checkbox.get_child(0)
+
+		checked.texture = checkbox_textures[1]
+		checked.visible = false
 
 func _unhandled_input(event: InputEvent) -> void:
 	var player_id = PM.get_player_id(event.device)
+	var player_index = PM.get_player_index(player_id)
+	var checked = player_ready_checkboxes[player_index].get_child(0)
 
 	if player_id != 0:
 		if event.is_echo():
 			return
 
-		if event.is_action_pressed("move_up%d" % player_id):
-			_change_monkey_color(player_id, true)
-		elif event.is_action_pressed("move_down%d" % player_id):
-			_change_monkey_color(player_id)
+		if !checked.visible:
+			if event.is_action_pressed("move_up%d" % player_id):
+				_change_monkey_color(player_id, true)
+			elif event.is_action_pressed("move_down%d" % player_id):
+				_change_monkey_color(player_id)
+
+			if event.is_action_pressed("select%d" % player_id):
+				checked.visible = true
+				ready_players += 1
+
+		if checked.visible:
+			if event.is_action_pressed("back%d" % player_id):
+					checked.visible = false
+					ready_players -= 1
+
+
+	if !_player_count():
+		if event.is_action_pressed("back"):
+			GM.state = GM.States.TITLE
+
+
+func _process(_delta: float) -> void:
+	pass
 
 
 func _on_player_joined(player_id: int, _device_id: int, player_index: int) -> void:
@@ -44,7 +79,7 @@ func _on_player_joined(player_id: int, _device_id: int, player_index: int) -> vo
 	get_tree().current_scene.add_child(monkey)
 	monkey.name = "Player %d" % player_id
 	monkey.global_position = sprite_location.global_position + Vector2(sprite_location.size.x / 2, 0)
-	monkey.sprite_frames = GM.get_next_available_sprite(player_id)
+	monkey.sprite_frames = PM.get_next_available_sprite(player_id)
 	monkey.animation = "idle"
 	monkey.texture_filter = CanvasItem.TEXTURE_FILTER_NEAREST
 	monkey.scale = Vector2(3, 3)
@@ -58,9 +93,20 @@ func _on_player_left(player_id: int, player_index: int) -> void:
 	if monkey:
 		monkey.queue_free()
 
+
 func _change_monkey_color(player_id: int, reverse: bool = false) -> void:
 	var monkey = get_tree().current_scene.get_node_or_null("Player %d" % player_id)
 
 	if monkey:
-		monkey.sprite_frames = GM.get_next_available_sprite(player_id, reverse)
+		monkey.sprite_frames = PM.get_next_available_sprite(player_id, reverse)
 		monkey.animation = "idle"
+
+
+func _player_count() -> int:
+	var count: int = 0
+
+	for player in PM.players:
+		if player["joined"]:
+			count += 1
+
+	return count
--- scripts/player_manager.gd
+++ scripts/player_manager.gd
@@ -5,6 +5,7 @@ signal player_joined(player_id: int, device_id: int, player_index: int)
 signal player_left(player_id: int, player_index: int)
 
 @export var max_players: int = 4
+@export var sprite_sheets: Array[Dictionary] = []
 
 @onready var monkey_scene: PackedScene = preload("res://scenes/monkey.tscn")
 
@@ -18,7 +19,7 @@ func _ready() -> void:
 	action_list = InputMap.get_actions()
 
 	for player in range(max_players):
-		players.append({ "player_id": player_number, "device_id": null, "joined": false, "monkey": null })
+		players.append({ "player_id": player_number, "device_id": null, "joined": false, "ready": false, "monkey": null , "sprite": null, "points": 0})
 		player_number += 1
 
 	Input.joy_connection_changed.connect(_on_joy_connection_changed)
@@ -29,7 +30,7 @@ func _unhandled_input(event: InputEvent) -> void:
 
 	if GM.state == GM.States.PLAYER_SELECTION:
 		if event is InputEventJoypadButton:
-			if event.button_index == JOY_BUTTON_START:
+			if event.button_index == JOY_BUTTON_START or event.button_index == JOY_BUTTON_A:
 				if !device_player_map.has(device_id):
 					for player in players:
 						if !player["joined"]:
@@ -48,11 +49,28 @@ func _unhandled_input(event: InputEvent) -> void:
 							_build_input_map(player_id, device_id)
 
 							player_joined.emit(player_id, device_id, player_index)
+							break
+
+		if player_id != 0:
+
+			if event.is_action_pressed("select%d" % player_id):
+				for player in players:
+					if player["player_id"] == player_id:
+						if !player["ready"]:
+							player["ready"] = true
 
 							break
 
-		if event.is_action_pressed("back%d" % player_id):
-			_remove_player(player_id, device_id)
+			if event.is_action_pressed("back%d" % player_id):
+				for player in players:
+					if player["player_id"] == player_id:
+						if player["ready"]:
+							player["ready"] = false
+
+							break
+						else:
+							_remove_player(player_id, device_id)
+							break
 
 	else:
 		return # MAX PLAYERS
@@ -74,16 +92,23 @@ func _remove_player(player_id: int, device_id: int) -> void:
 			player["device_id"] = null
 			player["monkey"] = null
 			player_index = players.find(player)
-
 			break
 
 	device_player_map.erase(device_id)
 
 	for action in action_list:
+		if (action.begins_with("ui_")):
+			continue
+
 		var player_action = action + str(player_id)
 
 		if InputMap.has_action(player_action):
 			InputMap.erase_action(player_action)
+
+	for sprite in sprite_sheets:
+		if sprite["player_id"] == player_id:
+			sprite["player_id"] = 0
+			break
 
 	player_left.emit(player_id, player_index)
 
@@ -93,17 +118,17 @@ func _build_input_map(player_id: int, device_id: int) -> void:
 	var current_action:StringName
 	var current_event:InputEvent
 
-	for action in range(action_list.size()):
-		if (action_list[action].begins_with("ui_")):
+	for action in action_list:
+		if (action.begins_with("ui_")):
 			continue
 
-		current_action = action_list[action] + str(player_id)
+		current_action = action + str(player_id)
 		InputMap.add_action(current_action)
 
-		action_event_list = InputMap.action_get_events(action_list[action])
+		action_event_list = InputMap.action_get_events(action)
 
-		for event in range(action_event_list.size()):
-			current_event = action_event_list[event].duplicate(true)
+		for event in action_event_list:
+			current_event = event.duplicate(true)
 			current_event.set_device(device_id)
 			print(current_event.device)
 			InputMap.action_add_event(current_action,current_event)
@@ -114,3 +139,41 @@ func get_player_id(device_id: int) -> int:
 		return device_player_map[device_id]
 	else:
 		return 0
+
+func get_player_index(player_id: int) -> int:
+	for i in range(players.size()):
+		if players[i]["player_id"] == player_id:
+			return i
+	return -1
+
+
+func get_next_available_sprite(player_id: int, reverse: bool = false) -> Resource:
+	var player_index: int = get_player_index(player_id)
+	var new_sprite: Resource
+	var sprite_array_length: int = sprite_sheets.size()
+	var current_index: int = -1
+
+	for i in range(sprite_array_length):
+		if sprite_sheets[i]["player_id"] == player_id:
+			current_index = i
+			break
+
+	if current_index != -1:
+		sprite_sheets[current_index]["player_id"] = 0
+
+	var found_index: int = -1
+	var offset: int = -1 if reverse else 1
+
+	for i in range(1, sprite_array_length):
+		var idx = posmod(current_index + (offset * i), sprite_array_length)
+		print(idx)
+		if sprite_sheets[idx]["player_id"] == 0:
+			found_index = idx
+			break
+
+	if found_index != -1:
+		sprite_sheets[found_index]["player_id"] = player_id
+		new_sprite = sprite_sheets[found_index]["frames"]
+		players[player_index]["sprite"] = new_sprite
+
+	return new_sprite
