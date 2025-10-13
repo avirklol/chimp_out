29af2904e1577b716d63e6f9f8ea05e8a2f9558f
Annand Virk <annandvirk@gmail.com>
2025-10-13 19:15:25 -0400

    Added RoundTitle class to manage round title UI elements, including labels for round and most valuable monkey. Introduced associated UID file for resource tracking.

---
+++ scripts/round_title.gd
@@ -0,0 +1,6 @@
+class_name RoundTitle
+extends Control
+
+@onready var round_label: RichTextLabel = %RoundLabel
+@onready var mvm_label: RichTextLabel = %MVMLabel
+@onready var mvm_location: Control = %MVMLocation
